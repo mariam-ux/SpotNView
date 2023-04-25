bc75666ba174cba490c1873f4ebc69d3f8ea1c7b
hsensbeh099 <husseinsbeih99@gmail.com>
2023-04-25 12:19:15 -0700

    25/4 tuesday

--- app/src/main/java/com/example/spotnview/CaptureAct.java
+++
@@ -1,4 +0,0 @@
-package com.example.spotnview;
-import com.journeyapps.barcodescanner.CaptureActivity;
-public class CaptureAct {
-}
--- app/src/main/java/com/example/spotnview/QRScanActivity.java
+++ app/src/main/java/com/example/spotnview/QRScanActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 import android.os.Bundle;
+import com.journeyapps.barcodescanner.CaptureActivity;
 
 public class QRScanActivity extends AppCompatActivity {
     Button btn_scan;
@@ -55,4 +56,8 @@ public class QRScanActivity extends AppCompatActivity {
 
         }
     });
+
+    private class CaptureAct {
+
     }
+}
